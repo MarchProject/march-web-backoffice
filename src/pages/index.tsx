a107f99914975d69a3f5560818f1f9915dee50cb
Peerapat Toprasert <peerapat.toprasert@gmail.com>
2023-03-13 00:04:20 +0700

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <Button variant="contained" color="success">
             Contained
           </Button>
-          <Button variant="text">Bright</Button>
+          <Button variant="text">Bright22</Button>
           <Button variant="outlined">Outlined</Button>
         </div>
       </div>
